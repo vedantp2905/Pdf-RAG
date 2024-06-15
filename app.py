7e5a207e8c5267e91afbbeabcabe0799725a4302
vedantp2905 <vedantpungliya29@gmail.com>
2024-06-15 15:51:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,81 +1,75 @@
 import os
 import asyncio
-import requests
-from typing import Type, Any
-from io import BytesIO
 import streamlit as st
+from io import BytesIO
 from docx import Document
 from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 from crewai import Agent, Task, Crew
 from crewai_tools import PDFSearchTool
+from PyPDF2 import PdfFileReader
 
+def generate_text(llm, question, pdf_bytes):
+    inputs = {'question': question, 'pdf_bytes': pdf_bytes}
 
-    
-def generate_text(llm, question):
-    
-    inputs = {'question': question}
-    
     rag_tool = PDFSearchTool(
-    config=dict(
-        llm=dict(
-            provider="google", # or google, openai, anthropic, llama2, ...
-            config=dict(
-                model="gemini-1.5-flash",
-                temperature=0.6,
-                # top_p=1,
-                # stream=true,
+        config=dict(
+            llm=dict(
+                provider="google",  # or google, openai, anthropic, llama2, ...
+                config=dict(
+                    model="gemini-1.5-flash",
+                    temperature=0.6,
+                    # top_p=1,
+                    # stream=true,
+                ),
             ),
-        ),
-        embedder=dict(
-            provider="google", # or openai, ollama, ...
-            config=dict(
-                model="models/embedding-001",
-                task_type="retrieval_document",
-                title="Embeddings",
+            embedder=dict(
+                provider="google",  # or openai, ollama, ...
+                config=dict(
+                    model="models/embedding-001",
+                    task_type="retrieval_document",
+                    title="Embeddings",
+                ),
             ),
-        ),
+        )
     )
-)
-    
+
     writer_agent = Agent(
-    role='Research Specialist',
-    goal='To accurately and efficiently retrieve and synthesize information from PDFs to answer user questions.',
-    backstory='''
-    Alex has always been passionate about research and information synthesis. With a background in library science 
-    and information technology, Alex developed a keen eye for finding precise information in vast databases and documents
-    . Joining the AI research team, Alex now uses advanced PDF retrieval tools to help users find accurate answers quickly.
-    Alex's meticulous nature and dedication to accuracy make them the perfect fit for the role of Research Specialist, ensuring
-    that every question is answered with reliable and well-sourced information.
-    ''',
-    verbose=True,
-    allow_delegation=False,
-    llm=llm
-)
+        role='Research Specialist',
+        goal='To accurately and efficiently retrieve and synthesize information from PDFs to answer user questions.',
+        backstory='''
+        Alex has always been passionate about research and information synthesis. With a background in library science 
+        and information technology, Alex developed a keen eye for finding precise information in vast databases and documents.
+        Joining the AI research team, Alex now uses advanced PDF retrieval tools to help users find accurate answers quickly.
+        Alex's meticulous nature and dedication to accuracy make them the perfect fit for the role of Research Specialist, ensuring
+        that every question is answered with reliable and well-sourced information.
+        ''',
+        verbose=True,
+        allow_delegation=False,
+        llm=llm
+    )
 
     task_writer = Task(
-    description=('''Use the PDF RAG search tool to accurately and efficiently retrieve and synthesize information from provided PDFs to answer user questions. 
-                 The task involves analyzing user queries, retrieving relevant information from the PDFs, and generating clear, concise, and accurate responses.'''),
-    agent=writer_agent,
-    expected_output="""
-    - A detailed and well-sourced answer to the user's question.
-    - References to specific sections or pages in the PDFs where the information was found.
-    - Clear and concise synthesis of the retrieved information, formatted in a user-friendly manner.
-    """,
-    tools=[rag_tool]
-)
+        description=('''Use the PDF RAG search tool to accurately and efficiently retrieve and synthesize information from provided PDFs to answer user questions. 
+                     The task involves analyzing user queries, retrieving relevant information from the PDFs, and generating clear, concise, and accurate responses.'''),
+        agent=writer_agent,
+        expected_output="""
+        - A detailed and well-sourced answer to the user's question.
+        - References to specific sections or pages in the PDFs where the information was found.
+        - Clear and concise synthesis of the retrieved information, formatted in a user-friendly manner.
+        """,
+        tools=[rag_tool]
+    )
 
-    
     crew = Crew(
         agents=[writer_agent],
         tasks=[task_writer],
         verbose=2
-        )
+    )
 
     result = crew.kickoff(inputs=inputs)
 
     return result
-
 
 def main():
     st.header('RAG Content Generator')
@@ -87,7 +81,7 @@ def main():
             api_key = st.text_input(f'Enter your API key', type="password")
             submitted = st.form_submit_button("Submit")
 
-    if api_key and serp_api_key:
+    if api_key and submitted:
         if model == 'OpenAI':
             async def setup_OpenAI():
                 loop = asyncio.get_event_loop()
@@ -121,32 +115,33 @@ def main():
 
             llm = asyncio.run(setup_gemini())
             mod = 'Gemini'
-        
-        question = st.text_input("Enter your question:")
 
-        if st.button("Generate Answer"):
-            with st.spinner("Generating Answer..."):
-                generated_content = generate_text(llm, question)
+        pdf_file = st.file_uploader("Upload your PDF file", type=["pdf"])
 
+        if pdf_file is not None:
+            question = st.text_input("Enter your question:")
 
+            if st.button("Generate Answer"):
+                with st.spinner("Generating Answer..."):
+                    pdf_bytes = pdf_file.read()
+                    generated_content = generate_text(llm, question, pdf_bytes)
 
-                st.markdown(generated_content)
+                    st.markdown(generated_content)
 
-                doc = Document()
-                
-                doc.add_heading(question)
-                doc.add_paragraph(generated_content)
+                    doc = Document()
+                    doc.add_heading(question)
+                    doc.add_paragraph(generated_content)
 
-                buffer = BytesIO()
-                doc.save(buffer)
-                buffer.seek(0)
+                    buffer = BytesIO()
+                    doc.save(buffer)
+                    buffer.seek(0)
 
-                st.download_button(
-                    label="Download as Word Document",
-                    data=buffer,
-                    file_name=f"{question}.docx",
-                    mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-                )
+                    st.download_button(
+                        label="Download as Word Document",
+                        data=buffer,
+                        file_name=f"{question}.docx",
+                        mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+                    )
 
 if __name__ == "__main__":
     main()
