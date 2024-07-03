0e13bb76c848a508fe312ed0cb642de156b50474
vedantp2905 <vedantpungliya29@gmail.com>
2024-07-03 11:47:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 from crewai import Agent, Task, Crew
 from crewai_tools import DirectorySearchTool
+from dotenv import load_dotenv
 
+load_dotenv('secret.env')
 
 # Function to handle RAG content generation
 def generate_text(llm, question, rag_tool):
@@ -101,10 +103,9 @@ mod = None
 with st.sidebar:
     with st.form('Gemini/OpenAI'):
         model = st.radio('Choose Your LLM', ['Gemini','OpenAI'])
-        api_key = st.text_input(f'Enter your API key', type="password")
         submitted = st.form_submit_button("Submit")
 
-if api_key:
+if model:
     if model == 'OpenAI':
         async def setup_OpenAI():
             loop = asyncio.get_event_loop()
@@ -112,9 +113,9 @@ if api_key:
                 loop = asyncio.new_event_loop()
                 asyncio.set_event_loop(loop)
 
-            os.environ["OPENAI_API_KEY"] = api_key
+            os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
              
-            llm = ChatOpenAI(model='gpt-4-turbo', temperature=0.6, max_tokens=200, api_key=api_key)
+            llm = ChatOpenAI(model='gpt-4-turbo', temperature=0.6, max_tokens=1000, api_key=os.getenv('OPENAI_API_KEY'))
             print("OpenAI Configured")
             return llm
 
@@ -128,13 +129,13 @@ if api_key:
                 loop = asyncio.new_event_loop()
                 asyncio.set_event_loop(loop)
 
-            os.environ["GOOGLE_API_KEY"] = api_key
+            os.environ["GOOGLE_API_KEY"] = load_dotenv('')
 
             llm = ChatGoogleGenerativeAI(
                 model="gemini-1.5-flash",
                 verbose=True,
                 temperature=0.6,
-                google_api_key=api_key
+                google_api_key=os.getenv('GOOGLE_API_KEY')
             )
             print("Gemini Configured")
             return llm
@@ -171,6 +172,6 @@ if api_key:
 
 
         # Display assistant response in chat message container
-        st.markdown(f"**Echo Bot**: {response}")
+        st.markdown(f"**Assistant**: {response}")
         # Add assistant response to chat history
         st.session_state.messages.append({"role": "assistant", "content": response})
